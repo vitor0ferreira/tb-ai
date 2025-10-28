c34387beb7c6127149766a815aa4dfb3b2bfb6d3
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2025-10-28 18:35:05 -0300

    fix: prisma issues

--- lib/generated/prisma-client/client.ts
+++ lib/generated/prisma-client/client.ts
@@ -43,6 +43,10 @@ export { Prisma }
 path.join(__dirname, "query_engine-windows.dll.node")
 path.join(process.cwd(), "lib/generated/prisma-client/query_engine-windows.dll.node")
 
+// file annotations for bundling tools to include these files
+path.join(__dirname, "libquery_engine-rhel-openssl-3.0.x.so.node")
+path.join(process.cwd(), "lib/generated/prisma-client/libquery_engine-rhel-openssl-3.0.x.so.node")
+
 /**
  * Model AnalysisLog
  * 
